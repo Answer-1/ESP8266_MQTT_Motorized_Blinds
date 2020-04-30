10fa0fcfb69877f3e2f59d71668cad16ab635a01
Answer <47510080+Answer-1@users.noreply.github.com>
2020-04-29 20:15:33 -0400

    Create LocalLanStuff.h

---
+++ LocalLanStuff.h
@@ -0,0 +1,6 @@
+#define USER_SSID                 "wifi-ssid"
+#define USER_PASSWORD             "wifi-pwd"
+#define USER_MQTT_SERVER          "192.168.0.1"
+#define USER_MQTT_PORT            1883
+#define USER_MQTT_USERNAME        "MosquittoUser"
+#define USER_MQTT_PASSWORD        "MosquittoPwd"
